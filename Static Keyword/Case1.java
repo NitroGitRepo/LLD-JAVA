203cf66e8dd2be1e722710e1a393f2d8805d30d6
NitroGitRepo <rajneesh.yadav.aws@gmail.com>
2024-06-07 19:27:31 +0530

    static keyword

---
+++ Static Keyword/Case1.java
@@ -0,0 +1,18 @@
+class Case1
+{  int a = 10;
+   void show()
+   {
+      System.out.println("Non Static Method");
+   }
+   static void display()
+   {   int a = this.a;
+	   System.out.println("Static Method"+a);
+   }
+   public static void main(String[] args)
+   {
+      Case1 c = new Case1();
+	  c.show();
+	  Case1.display();
+	  display();
+   }
+}
---
+++ Static Keyword/Case2.java
@@ -0,0 +1,12 @@
+class Case2
+{
+    static 
+	{
+	   System.out.println("Static Block");
+	}
+
+    public static void main(String[] args)
+	{ 
+	   System.out.println("Main Method");
+    }
+}
