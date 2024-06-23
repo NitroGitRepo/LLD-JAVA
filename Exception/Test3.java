44ea023c1f001bb5a74f786e2161559c9a489a1d
NitroGitRepo <rajneesh.yadav.aws@gmail.com>
2024-06-23 19:52:24 +0530

    exception handled using try catch

--- Exception/Test3.java
+++ Exception/Test3.java
@@ -3,12 +3,17 @@ class Test3
 
     public static void main(String[] args){
 	     int a  =100;
-		 int b = 0;
+		 int b = 10;
 		 int c;
 		 
 		 try{
+		 System.out.println("1");
+		 System.out.println("2");
+		 System.out.println("3");
 		 c = a/b;
 		 System.out.println(c);
+		 System.out.println("4");
+		 System.out.println("5");
 		 }
 		 catch(Exception e){
 		  System.out.println(e); 
